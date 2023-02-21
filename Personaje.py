7a0458a7b64c91c21d2627b552ffdfccb9413a86
JuAnMoRa49 <125929793+JuAnMoRa49@users.noreply.github.com>
2023-02-20 18:43:20 -0600

    Definicion de metodos

--- Personaje.py
+++ Personaje.py
@@ -9,4 +9,17 @@ class Personaje:
     
     #Metodos Personaje
     
-    
+    def correr(self, status):
+        if(status):
+            print("El personaje " , self.nombre , " está corriendo")
+        else:
+            print("El personaje " , self.nombre , " se detuvo")
+            
+    def lanzarGranada(self):
+        print("Se lanzo granada ")
+        
+    def recargarArma(self, municiones):
+        cargador = 5
+        cargador = cargador + municiones
+        print("El arma tiene ahora " , cargador , " balas")
+        
